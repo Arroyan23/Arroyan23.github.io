922b0ef98d92ed9fe5c254c5be98b0e9529f8377
740195 <740195@38rqq6l7_740195.telkom.co.id>
2021-06-18 21:30:18 +0700

    membuat responsive belum selesai

---
+++ main.js
@@ -0,0 +1,13 @@
+//selsection for class
+
+function kelas1(){
+    location.href = "mencariNilai/mencari.html";
+}
+
+function kelas2(){
+    location.href = "financeManagement/finance.html";
+}
+
+function kelas3(){
+    location.href = "ExpenseTracker/expense.html";
+}
