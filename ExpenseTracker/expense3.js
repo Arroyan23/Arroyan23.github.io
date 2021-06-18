35de4d3e1919b7db91add4156983f30eab74f48e
arroyan23 <syawqiarroyan@gmail.com>
2021-06-18 11:30:04 +0700

    menyelesaikan project Expense Tacker(Revisi = mediaquery)

--- ExpenseTracker/expense3.js
+++ ExpenseTracker/expense3.js
@@ -1,29 +1,51 @@
 // mengambil value dari inputan
 
-const input1 = document.querySelector('.pemasukkan-input .form-name');
-const input2 = document.querySelector('.pemasukkan-input .form-date');
-const input3 = document.querySelector('.pemasukkan-input .form-number');
-const button1 = document.querySelector('.pemasukkan-input button');
+const input1 = document.querySelector(".pemasukkan-input .form-name");
+const input2 = document.querySelector(".pemasukkan-input .form-date");
+const input3 = document.querySelector(".pemasukkan-input .form-number");
+const button1 = document.querySelector(".pemasukkan-input button");
 
-//selector ul li 
+//selector ul li
 
-const ulLi = document.querySelector('.logic2')
+const ulLi = document.querySelector(".container2 table tbody");
 
 //menambahkan fungsi untuk di pencet button
 
-button1.addEventListener('click', function() {
-    var value1 = input1.value;
-    var value2 = input2.value;
-    var value3 = input3.value;
+//fungsi untuk popup
 
-    
-    
-        ulLi.innerHTML += `
-            <ul>
-                <li>${value1}</li>
-                <li>${value2}</li>
-                <li>${value3}</li>
-            </ul>
-        `
-    
+button1.addEventListener("click", function () {
+  var value1 = input1.value;
+  var value2 = input2.value;
+  var value3 = input3.value;
+  if (value1 === "" || value2 === "" || value3 === "") {
+    var ab = confirm("Please insert a value");
+    if (ab === true) {
+    } else {
+      location.href = "https://api.whatsapp.com/send?phone=6281218012006";
+      alert("Ok get out");
+    }
+  } else {
+    ulLi.innerHTML += `
+            <tr class="tbdb">
+                <td>${value1}</td>
+                <td>${value2}</td>
+                <td>${value3}</td>
+                <td class = "delete"><i class="bi bi-trash-fill"></i></td>
+            </tr>
+        
+        `;
+  }
+
+  const del1 = document.querySelectorAll(".delete");
+  var i;
+
+  for (i = 0; i < del1.length; i++) {
+    del1[i].onclick = function () {
+      this.parentNode.style.display = "none";
+    };
+  }
+
+  input1.value = "";
+  input2.value = "";
+  input3.value = "";
 });
